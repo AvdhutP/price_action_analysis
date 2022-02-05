dba1cf62b31d98f4676dfcaabb7a165032d426b5
AvdhutP <>
2022-02-05 16:04:51 +0000

    taking ticker from console and printing it

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import yfinance as yf
+import sys
+
+# Set the ticker
+ticker = sys.argv[0]
+
+print(ticker)
 
 # Set the start and end date
 start_date = '2021-01-01'
 end_date = '2021-12-31'
-
-# Set the ticker
-ticker = 'TCS'
 
 # Get the data
 data = yf.download(ticker, start_date, end_date)
